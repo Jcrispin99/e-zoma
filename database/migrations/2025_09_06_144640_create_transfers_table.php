ccb51e5ddf496e123daf8aca214ea35892cdbf48
Jhamil Antony Crispin Martel <wild@MacBook-Pro-de-Jhamil.local>
2025-09-06 15:31:20 -0500

    Migraciones BD, pendiente modificar la bd para que trabaje con variantes y sea exacto mas no con un producto como plantilla

---
+++ database/migrations/2025_09_06_144640_create_transfers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('serie');
+            $table->string('correlative');
+
+            $table->timestamp('date');
+
+            $table->string('observation')->nullable();
+
+            $table->decimal('total', 10, 2)->default(0.00);
+
+            $table->foreignId('origin_warehouse_id')->constrained('warehouses')->onDelete('cascade');
+
+            $table->foreignId('desination_warehouse_id')->constrained('warehouses')->onDelete('cascade');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transfers');
+    }
+};

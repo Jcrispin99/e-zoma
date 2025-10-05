d088c7f87ad286ac9ae08827220c2bbca414d8e8
Jhamil Antony Crispin Martel <wild@MacBook-Pro-de-Jhamil.local>
2025-10-05 06:42:44 -0500

    Index Warehouse company

--- app/Livewire/Admin/Datatables/WarehouseTable.php
+++ app/Livewire/Admin/Datatables/WarehouseTable.php
@@ -5,10 +5,21 @@ namespace App\Livewire\Admin\Datatables;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Warehouse;
+use App\Livewire\Traits\CompanyFilterable;
+use Illuminate\Database\Eloquent\Builder;
 
 class WarehouseTable extends DataTableComponent
 {
+    use CompanyFilterable;
+
     protected $model = Warehouse::class;
+
+    protected $listeners = ['company-changed' => '$refresh'];
+
+    public function builder(): Builder
+    {
+        return $this->applyCompanyFilter(Warehouse::query());
+    }
 
     public function configure(): void
     {
@@ -28,7 +39,7 @@ class WarehouseTable extends DataTableComponent
                 ->label(function ($row, Column $column) {
                     return view('admin.warehouses.actions', ['warehouse' => $row]);
                 }),
-            
+
         ];
     }
 }
